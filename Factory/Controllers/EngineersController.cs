55ee94753c88c88d521719a73ecdc03f49599790
Constantine <faustlarsen@gmail.com>
2021-01-08 09:55:33 -0700

    add controllers and models files

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+  }
+}
---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+  
+  }
+}
