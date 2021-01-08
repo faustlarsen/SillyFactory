9893205123a4bf1254143c914dbfa3e148b080a7
Constantine <faustlarsen@gmail.com>
2021-01-08 10:25:38 -0700

    add models code

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
+using System;
 
 namespace Factory.Models
 {
-  public class Engineer
-  {
-
-  }
+    public class Engineer
+    {
+        public Engineer()
+        {
+          this.Machines = new HashSet<EngineerMachine>();
+        }
+        public int EngineerId {get; set;}
+        public string Name {get;set;}
+        public virtual ICollection<EngineerMachine> Machines  {get; set;}
+    } 
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Factory.Models
 {
-  public class Machine
-  {
-    
-  }
+    public class Machine
+    {
+        public Machine()
+        {
+          this.Engineers = new HashSet<EngineerMachine>();
+        }
+        public int MachineId  {get;set;}
+        public string Name {get; set;}
+        public virtual ICollection<EngineerMachine> Engineers {get; set;}
+    }
 }
